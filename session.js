b507b63fa27e7f3b6e70f0ca946147936775db51
haneul <kehoecicco@live.fr>
2019-01-16 11:09:07 +0100

    server

---
+++ session.js
@@ -0,0 +1,22 @@
+class Session {
+    constructor() {
+        this.sessionA = new Map();
+        this.sessionB = new Map();
+
+        this.sessions = new Map();
+        this.sessions.set('A', this.sessionA);
+        this.sessions.set('B', this.sessionB);
+    }
+
+    add(team, pseudo, socket) {
+        let map = this.sessions.get(team);
+        if (map.size < 3) {
+            map.set(pseudo, socket);
+            return true;
+        }
+        return false;
+    }
+
+}
+
+module.exports = Session;
